2e84b88ed8eccbe430d9766cfdcd6059497e469f
Dillion Megida <dillionmegida@gmail.com>
2024-01-31 13:48:05 +0100

    update with demo feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["res.cloudinary.com"],
+    service: passthroughImageService()
   }
 });
--- src/cloudinary.js
+++
@@ -1,8 +0,0 @@
-import {v2 as cloudinary} from 'cloudinary'
-
-cloudinary.config({
-	cloud_name: "dillionmegida",
-	secure: true,
-});
-
-export const cld = cloudinary
